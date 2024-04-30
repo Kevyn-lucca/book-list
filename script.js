accb065a03fcf9ee0a5f0d36f451e6a5b6d60fab
kevyn <kevynalternativo@gmail.com>
2024-04-30 17:30:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+
+
+//livros no form devem ter: author, titulo, numero de paginas, se ja foi lido ou não, para pontos bonus, uma breve descrição.
+
+function Book() {
+  
+}
+
+//essa função deve pegar livros apartir do input do usuario e criar adicionar eles ao array
+
+function addBookToLibrary() {
+  
+}
